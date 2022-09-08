e41aed7d534ecfd29fac968fcde118da5a10b5d6
rahulnr <nrrahul4@gmail.com>
2022-09-08 16:28:25 +0530

    Basic authentication in Spring security when WebSecurityConfigurerAdapter deprecated.

--- src/main/java/com/example/Authenticator/Security/SecurityConfig.java
+++ src/main/java/com/example/Authenticator/Security/SecurityConfig.java
@@ -1,27 +1,46 @@
 package com.example.Authenticator.Security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.constant.Constable;
+import java.util.function.Function;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+    protected static final String ENCODED_PWD = "$2a$10$KtphGNMgY4TgmW9ZiYNsI.H/2OgDniiGw8.1ia9neDS8hy8iD9h22";
 
-    @Autowired
-    CustomAuthProvider customAuthProvider;
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider((AuthenticationProvider) customAuthProvider);
+    @Bean
+    public PasswordEncoder bcryptEncoder() {
+        return new BCryptPasswordEncoder();
     }
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers("/api/getSample").authenticated();
+        UserDetails user =
+                User.withUsername("Rahul")
+                        .password(ENCODED_PWD).roles()
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/getSample")
+                .authenticated();
+
+        return http.build();
     }
 }
--- src/main/java/com/example/Authenticator/Services/SampleTestService.java
+++ src/main/java/com/example/Authenticator/Services/SampleTestService.java
@@ -1,5 +1,7 @@
 package com.example.Authenticator.Services;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class SampleTestService {
 
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @GetMapping("/tester")
     public String tester() {
-        return "No Auth required";
+        return passwordEncoder().encode("1234").toString();
     }
 }
